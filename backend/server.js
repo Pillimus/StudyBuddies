b9388f2b7a4866cdc04b7f3a7c7b50b37a4f6f63
eeveelife13 <eeveelife14@gmail.com>
2026-04-03 23:09:47 -0400

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,127 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-var cardList = [
-  "Roy Campanella",
-  "Paul Molitor",
-  "Tony Gwynn",
-  "Dennis Eckersley",
-  "Reggie Jackson",
-  "Gaylord Perry",
-  "Buck Leonard",
-  "Rollie Fingers",
-  "Charlie Gehringer",
-  "Wade Boggs",
-  "Carl Hubbell",
-  "Dave Winfield",
-  "Jackie Robinson",
-  "Ken Griffey, Jr.",
-  "Al Simmons",
-  "Chuck Klein",
-  "Mel Ott",
-  "Mark McGwire",
-  "Nolan Ryan",
-  "Ralph Kiner",
-  "Yogi Berra",
-  "Goose Goslin",
-  "Greg Maddux",
-  "Frankie Frisch",
-  "Ernie Banks",
-  "Ozzie Smith",
-  "Hank Greenberg",
-  "Kirby Puckett",
-  "Bob Feller",
-  "Dizzy Dean",
-  "Joe Jackson",
-  "Sam Crawford",
-  "Barry Bonds",
-  "Duke Snider",
-  "George Sisler",
-  "Ed Walsh",
-  "Tom Seaver",
-  "Willie Stargell",
-  "Bob Gibson",
-  "Brooks Robinson",
-  "Steve Carlton",
-  "Joe Medwick",
-  "Nap Lajoie",
-  "Cal Ripken, Jr.",
-  "Mike Schmidt",
-  "Eddie Murray",
-  "Tris Speaker",
-  "Al Kaline",
-  "Sandy Koufax",
-  "Willie Keeler",
-  "Pete Rose",
-  "Robin Roberts",
-  "Eddie Collins",
-  "Lefty Gomez",
-  "Lefty Grove",
-  "Carl Yastrzemski",
-  "Frank Robinson",
-  "Juan Marichal",
-  "Warren Spahn",
-  "Pie Traynor",
-  "Roberto Clemente",
-  "Harmon Killebrew",
-  "Satchel Paige",
-  "Eddie Plank",
-  "Josh Gibson",
-  "Oscar Charleston",
-  "Mickey Mantle",
-  "Cool Papa Bell",
-  "Johnny Bench",
-  "Mickey Cochrane",
-  "Jimmie Foxx",
-  "Jim Palmer",
-  "Cy Young",
-  "Eddie Mathews",
-  "Honus Wagner",
-  "Paul Waner",
-  "Grover Alexander",
-  "Rod Carew",
-  "Joe DiMaggio",
-  "Joe Morgan",
-  "Stan Musial",
-  "Bill Terry",
-  "Rogers Hornsby",
-  "Lou Brock",
-  "Ted Williams",
-  "Bill Dickey",
-  "Christy Mathewson",
-  "Willie McCovey",
-  "Lou Gehrig",
-  "George Brett",
-  "Hank Aaron",
-  "Harry Heilmann",
-  "Walter Johnson",
-  "Roger Clemens",
-  "Ty Cobb",
-  "Whitey Ford",
-  "Willie Mays",
-  "Rickey Henderson",
-  "Babe Ruth",
-];
-
 const MongoClient = require("mongodb").MongoClient;
 const url =
-  "mongodb+srv://RickLeinecker:COP4331Rocks@cluster0-4pisv.mongodb.net/COP4331?retryWrites=true&w=majority";
+  "mongodb+srv://<db_username>:<db_password>@mernproject.jqcxaqy.mongodb.net/?appName=MernProject";
 const client = new MongoClient(url);
 client.connect();
 
-const app = express();
-app.use(cors());
-// app.use(bodyParser.json());
-app.use(express.json());
-
-app.post("/api/addcard", async (req, res, next) => {
+app.post("/api/addTask", async (req, res, next) => {
   // incoming: userId, color
   // outgoing: error
-  const { userId, card } = req.body;
-  const newCard = { Card: card, UserId: userId };
+  const { userId, task, deadline } = req.body;
+  const newTask = { Task: task, Deadline: deadline, UserId: userId };
   var error = "";
   try {
-    const db = client.db("COP4331Cards");
-    const result = db.collection("Cards").insertOne(newCard);
+    const db = client.db("Users");
+    const result = db.collection("Tasks").insertOne(newTard);
   } catch (e) {
     error = e.toString();
   }
@@ -153,15 +44,15 @@ app.post("/api/login", async (req, res, next) => {
   res.status(200).json(ret);
 });
 
-app.post("/api/searchcards", async (req, res, next) => {
+app.post("/api/searchTasks", async (req, res, next) => {
   // incoming: userId, search
   // outgoing: results[], error
   var error = "";
   const { userId, search } = req.body;
   var _search = search.trim();
-  const db = client.db("COP4331Cards");
+  const db = client.db("Users");
   const results = await db
-    .collection("Cards")
+    .collection("Tasks")
     .find({ Card: { $regex: _search + ".*", $options: "i" } })
     .toArray();
   var _ret = [];
