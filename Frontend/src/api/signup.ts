73d522d26482b46949c9f469699771afac5b1157
eeveelife13 <eeveelife14@gmail.com>
2026-04-11 12:32:17 -0400

    signup.ts converted

--- Frontend/src/api/signup.ts
+++ Frontend/src/api/signup.ts
@@ -6,14 +6,7 @@ export const signup = async (
   password: string,
 ) => {
   try {
-    const res = await fetch("http://study-buddies.me/api/signup", {
-=======
-import { buildApiUrl } from "./config";
-
-export const signup = async (name: string, lastName: string, email: string, password: string) => {
-  try {
     const res = await fetch(buildApiUrl("/api/signup"), {
->>>>>>> 78d47d01b7909a97f5f4c31ef167ca8eb0afc4a6
       method: "POST",
       headers: {
         "Content-Type": "application/json",
