69a734185a330ea02bdeaa251db1de82946d70f5
Denzel Galang <160177670+denzel-galang@users.noreply.github.com>
2026-04-12 02:53:38 -0400

    corrected jest.config.ts

---
+++ backend/jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.js"],
+  resetModules: true,
+};
+
+ // setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
